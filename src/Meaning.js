cfab72daf604389d6d7c1e303a41ed39226861b4
kikicod3r <kitajjenkins@gmail.com>
2023-10-13 17:59:59 -0500

    Finished definition, example, part of speech

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,17 +4,6 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech} </h3>
-      {props.meanings.definition.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              {definition.example}
-            </p>
-          </div>
-        );
-      })}
       <p>{props.meaning.definition} </p>
       <p>example: {props.meaning.example} </p>
     </div>
